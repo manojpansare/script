29067dfa34e619446e3115d7bd0ea5337f34a128
manojpansare <manojpansare2007@gmail.com>
2016-11-20 15:58:49 +0530

    Server Status

---
+++ serverstatus.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "##################################################################"
+echo -e "Status of \e[0;34;47m'$(hostname)'\e[0m server on '$(date +"%d-%m-%Y %H:%M")'"
+echo "##################################################################"
+echo -e "\e[0;34;47m Server config : \e[0m "
+echo "----------------"
+echo "Processor : "
+echo "$(grep "model name\|cpu MHz" /proc/cpuinfo)"
+echo "Memory : "
+
